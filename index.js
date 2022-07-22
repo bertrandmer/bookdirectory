e225242681110a7165df25662e343b6ee5f57732
bertrandmer <97544521+bertrandmer@users.noreply.github.com>
2022-07-21 21:52:25 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 5000;
+app.use(bodyParser.json());
+app.use('/api/v1', api);
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
